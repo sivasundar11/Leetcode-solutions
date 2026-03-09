0e651273c940d7772a0d739e0974d4455675ee99
sivasundar11 <151825011+sivasundar11@users.noreply.github.com>
2026-03-09 09:46:53 +0530

    Time: 260 ms (32.89%) | Memory: 47.8 MB (32.89%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    int MOD = 1000000007;
+    long[][][] dp;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+
+        dp = new long[zero + 1][one + 1][2];
+
+        for (int i = 0; i <= zero; i++)
+            for (int j = 0; j <= one; j++)
+                dp[i][j][0] = dp[i][j][1] = -1;
+
+        return (int)((solve(zero, one, 0, limit) + solve(zero, one, 1, limit)) % MOD);
+    }
+
+    private long solve(int z, int o, int last, int limit) {
+
+        if (z == 0 && o == 0) return 1;
+
+        if (dp[z][o][last] != -1) return dp[z][o][last];
+
+        long ans = 0;
+
+        if (last == 0) { // last placed was 0
+            for (int i = 1; i <= limit && i <= o; i++) {
+                ans = (ans + solve(z, o - i, 1, limit)) % MOD;
+            }
+        } else { // last placed was 1
+            for (int i = 1; i <= limit && i <= z; i++) {
+                ans = (ans + solve(z - i, o, 0, limit)) % MOD;
+            }
+        }
+
+        return dp[z][o][last] = ans;
+    }
+}
